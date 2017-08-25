15da8f16c30e686ed04afad9e66e978f6def4446
s1lv3rj1nx <pratamesh1867@gmail.com>
2017-08-25 12:18:39 +0530

    Added imagein options. Added function to get and save path for music directory.

--- break.py
+++ break.py
@@ -1,7 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
-import subprocess
-import webbrowser
+from tkinter.filedialog import askdirectory
+import subprocess, sys, webbrowser
+import PIL 
+from PIL import Image
+from PIL import ImageTk
 
 root = Tk()
 root.title("Break Reminder")
@@ -11,9 +14,13 @@ f = "times", 14, "italic"
 b = "arial", 10, "bold"
 v = IntVar()
 
-
 choices = [ ('Music',1), ('Motivate',2),('Ignite',3), ('Nothing',4)]
 
+def song_path():
+	path = askdirectory() 
+	f = open("sng_pth", "w")
+	f.write(path)
+	f.close()
 
 def ok():
 
@@ -30,13 +37,18 @@ def ok():
 	window = Toplevel() #for child window
 	window.geometry("600x250+1000+300")
 	window.title("TIME FOR BREAK")
-	Label(window, text = 'What you want to do?',padx=25,justify=LEFT,font = (f)).pack(anchor=W)
-	
+	Label(window, text = 'What you want to do?',padx=25,justify=LEFT,font = (f)).grid(sticky = 'W')
+
+	img = Image.open("aa.ico")
+	img = img.resize((150, 150), Image.ANTIALIAS)
+	photoImg = ImageTk.PhotoImage(img)
+	imglabel = Label(window, image=photoImg).place(x = 350, y = 45)
+
 	def task():
 		nonlocal breaks_consumed 
 		nonlocal total_breaks_allowed
 		if (breaks_consumed >= total_breaks_allowed):
-			messagebox.showinfo(title = 'Reminder', message= 'Timer Ended!!')
+			messagebox.showinfo(title = 'End', message= 'Timer Ended!! ')
 			window.destroy()
 			root.destroy()
 		else:
@@ -53,56 +65,69 @@ def ok():
 			temp = temp - 1000
 		messagebox.showinfo(title = 'Reminder', message= 'Break Over Back To Work!!')
 		return ( task() )
-		
-	
-	
+
 	def options():
 		x = v.get()
 		if x == 1:
-			subprocess.Popen( ["xdg-open", '/home/prathamesh/Music/']) 
+			path = open("sng_pth","r")
+
+			if sys.platform == 'darwin':
+				subprocess.Popen(['open', '--', path.read()])
+			elif sys.platform == 'linux':
+				subprocess.Popen(['xdg-open', path.read()])
+			elif sys.platform == 'win32':
+				subprocess.Popen(['explorer', path.read()])
+
+			path.close()
 			window.withdraw()
 			return ( process() )
+
 		if x == 2:
 			webbrowser.open("https://www.youtube.com/channel/UCf9_s9ii6BZ-klpgmtIi3WQ") 
 			window.withdraw()
 			return ( process() )
+
 		if x == 3:
 			webbrowser.open("http://www.geeksforgeeks.org/") 
 			window.withdraw()
 			return ( process() )
+			
 		if x == 4:
 			window.withdraw()
 			return ( process() )
 	
 
 	for txt, val in choices:
-			Radiobutton(window, text = txt, padx = 25, variable = v, value = val,font = (f), command = options).pack(anchor=W)
+			Radiobutton(window, text = txt, padx = 25, variable = v, value = val,font = (f), command = options).grid(sticky = 'W')
 
-	
 	window.mainloop()
 	
-	
 Label(root, text="Work Time (min)", font = (f)).grid(row = 0,sticky = W)
-workTime = Entry(root, font = (f))
+workTime = Entry(root, font = (f), justify = RIGHT)
 workTime.insert(0, 20)
 workTime.grid(row = 0, column = 1)
 
 Label(root, text="Break After (min)", font = (f)).grid(row = 1,sticky = W)
-breakAfter = Entry(root, font = (f))
+breakAfter = Entry(root, font = (f), justify = RIGHT)
 breakAfter.insert(0, 5)
 breakAfter.grid(row = 1, column = 1)
 
 Label(root, text="Break Duration (min)", font = (f)).grid(row = 2, sticky = W)
-breakDuration= Entry(root, font = (f))
+breakDuration= Entry(root, font = (f), justify = RIGHT)
 breakDuration.insert(0,2)
 breakDuration.grid(row = 2, column = 1)
+
+mymenu = Menu() #To create menu object
+list1 = Menu() #To create list of menus
+list1.add_command(label = 'Defaults', command = song_path)
+mymenu.add_cascade(label = 'Options', menu =list1)
+
+root.config(menu = mymenu)
 
 buttton1 = Button(root, text = "OK", command = ok,font = (b) )
 buttton1.grid(row = 4, column = 1)
 
 button2 = Button(root, text = "QUIT", command = root.destroy,font = (b) )
 button2.grid(row = 4, column = 0)
-
-
 
 root.mainloop()

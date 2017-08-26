2640d9dbeb28341e75d2573a4a39cc1083ec9ec3
Prathamesh Saraf <pratamesh1867@gmail.com>
2017-08-26 17:41:38 +0530

    Add files via upload

--- break.py
+++ break.py
@@ -6,7 +6,6 @@ import PIL
 from PIL import Image
 from PIL import ImageTk
 
-
 root = Tk()
 root.title("Break Reminder")
 root.geometry("550x180+500+250")
@@ -25,6 +24,7 @@ def song_path():
 
 def ok():
 
+	#set to 1000 for faster debugging 
 	wT = int(workTime.get())*1000*60
 	bA = int(breakAfter.get())*1000*60
 	
@@ -38,12 +38,6 @@ def ok():
 	window = Toplevel() #for child window
 	window.geometry("480x240+500+250")        
 	window.title("TIME FOR BREAK")
-	try:
-          import winsound
-          winsound.PlaySound("analog-watch.wav")
-	except ImportError:
-          pass
-      
         
 	Label(window, text = 'What you want to do?',padx=25,justify=LEFT,font = (f)).grid(sticky = 'W')
 
@@ -66,6 +60,7 @@ def ok():
 			breaks_consumed = breaks_consumed + 1
 
 	def process():
+		#set to 1000 for faster debugging
 		bD = int(breakDuration.get())*1000*60
 		temp = bD
 		while temp > 0:
@@ -107,17 +102,24 @@ def ok():
 
 	for txt, val in choices:
 			Radiobutton(window, text = txt, padx = 25, variable = v, value = val,font = (f), command = options).grid(sticky = 'W')
-
+			
+	try:
+		import winsound  #for windows system to play alaram
+		winsound.PlaySound('analog-watch.wav',winsound.SND_FILENAME)
+	except ImportError:
+		pass
+    
+	window.resizable(False, False)
 	window.mainloop()
 	
 Label(root, text="Work Time (min)", font = (f)).grid(row = 0,sticky = W)
 workTime = Entry(root, font = (f), justify = RIGHT)
-#workTime.insert(0, 0)
+#workTime.insert(0, 0)  #set some default values for trial checking
 workTime.grid(row = 0, column = 1)
 
 Label(root, text="Break After (min)", font = (f)).grid(row = 1,sticky = W)
 breakAfter = Entry(root, font = (f), justify = RIGHT)
-#breakAfter.insert(0, 1)
+#breakAfter.insert(0, 1)  
 breakAfter.grid(row = 1, column = 1)
 
 Label(root, text="Break Duration (min)", font = (f)).grid(row = 2, sticky = W)
@@ -138,4 +140,5 @@ buttton1.grid(row = 4, column = 1)
 button2 = Button(root, text = "QUIT", command = root.destroy,font = (b) )
 button2.grid(row = 4, column = 0)
 
+root.resizable(False, False)
 root.mainloop()
